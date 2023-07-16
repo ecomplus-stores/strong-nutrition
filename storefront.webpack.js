f576adeeb9c052d0fbb3f8656188b5ebfad624d0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-07-16 18:13:19 -0300

    chore: update componets chage

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,8 +3,6 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js')
     }
   }
